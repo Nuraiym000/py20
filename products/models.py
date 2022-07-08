35de2386ac9d251e3c97019285f6f7acfe31ff75
Nuraiym000 <nurajymmm1@gmail.com>
2022-07-08 14:52:35 +0600

    commit

---
+++ products/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, 
+                                 on_delete=models.CASCADE, 
+                                 related_name='products')
+    image = models.ImageField(upload_to='products')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment from {self.author.name} to {self.product}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created_at']
